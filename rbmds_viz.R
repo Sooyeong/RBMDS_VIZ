2fd6e62dc0c61f6a781cce95378249d2f4941bd9
Sooyeong <sooyeonglim@sooyeongs-mbp.home>
2020-09-26 12:40:25 -0400

    rbmds_viz.R

---
+++ rbmds_viz.R
@@ -0,0 +1,11 @@
+# RBMDS- Data visualization
+# Author: Sooyeong Lim
+# Date 2020/09/26
+# Will use Shiny, ggplot, plotly() objects
+# ggplot, plotly work station 
+
+
+# load ToxicR package
+
+
+bkskx
